51c587cbda7450e4e7b4a993188a032c78b54bbd
Thiago de Sousa Pereira <thdesous@gmail.com>
2024-03-26 16:51:50 -0300

    feat: added data source aws_region in availability_zone

--- infra/subnet-public.tf
+++ infra/subnet-public.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "subnet-pub-vpc-eks-1a" {
   vpc_id                  = aws_vpc.vpc-eks.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
-  availability_zone       = "us-east-1a"
+  availability_zone       = "${data.aws_region.region-current.name}a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -13,7 +13,7 @@ resource "aws_subnet" "subnet-pub-vpc-eks-1a" {
 resource "aws_subnet" "subnet-pub-vpc-eks-1b" {
   vpc_id                  = aws_vpc.vpc-eks.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
-  availability_zone       = "us-east-1b"
+  availability_zone       = "${data.aws_region.region-current.name}b"
   map_public_ip_on_launch = true
 
   tags = {
