4adcebf86cef276ec51b8ddfe42f27abb8214e63
Thiago de Sousa Pereira <thdesous@gmail.com>
2024-03-27 20:50:52 -0300

    feat: created private route table with route for public nat gateway

---
+++ infra/route-table-private.tf
@@ -0,0 +1,31 @@
+resource "aws_route_table" "route-table-pri-1a" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat-gateway-pub-1a.id
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-route-table-pri-1a"
+    }
+  )
+}
+
+resource "aws_route_table" "route-table-pri-1b" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat-gateway-pub-1b.id
+  }
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-route-table-pri-1b"
+    }
+  )
+}
