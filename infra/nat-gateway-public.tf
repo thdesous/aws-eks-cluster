0ee254e3c8f3fd23113d30bfe32a7e53b4e6526e
Thiago de Sousa Pereira <thdesous@gmail.com>
2024-03-27 20:48:31 -0300

    feat: created nat gateway resource and associated with elastic ip resource

---
+++ infra/nat-gateway-public.tf
@@ -0,0 +1,23 @@
+resource "aws_nat_gateway" "nat-gateway-pub-1a" {
+  allocation_id = aws_eip.elastic-ip-1a.id
+  subnet_id     = aws_subnet.subnet-pub-1a.id
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-nat-gateway-pub-1a"
+    }
+  )
+}
+
+resource "aws_nat_gateway" "nat-gateway-pub-1b" {
+  allocation_id = aws_eip.elastic-ip-1b.id
+  subnet_id     = aws_subnet.subnet-pub-1b.id
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-nat-gateway-pub-1b"
+    }
+  )
+}
