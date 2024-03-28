4b97fe5a2192d6c0e61a677d9e228cb3a59996ea
Thiago de Sousa Pereira <thdesous@gmail.com>
2024-03-27 21:03:56 -0300

    feat: associated private subnet with private route table

--- infra/subnet-private.tf
+++ infra/subnet-private.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "subnet-priv-1a" {
+resource "aws_subnet" "subnet-pri-1a" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = cidrsubnet(var.cidr_block, 8, 3)
   availability_zone = "${data.aws_region.region-current.name}a"
@@ -6,13 +6,13 @@ resource "aws_subnet" "subnet-priv-1a" {
   tags = merge(
     local.tags,
     {
-      Name                              = "${var.project_name}-subnet-priv-1a"
+      Name                              = "${var.project_name}-subnet-pri-1a"
       "kubernetes.io/role/internal-elb" = 1
     }
   )
 }
 
-resource "aws_subnet" "subnet-priv-1b" {
+resource "aws_subnet" "subnet-pri-1b" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = cidrsubnet(var.cidr_block, 8, 4)
   availability_zone = "${data.aws_region.region-current.name}b"
@@ -20,8 +20,20 @@ resource "aws_subnet" "subnet-priv-1b" {
   tags = merge(
     local.tags,
     {
-      Name                              = "${var.project_name}-subnet-priv-1b"
+      Name                              = "${var.project_name}-subnet-pri-1b"
       "kubernetes.io/role/internal-elb" = 1
     }
   )
+}
+
+# Provides a resource to create an association between a route table and a subnet
+
+resource "aws_route_table_association" "route-table-ass-pri-1a" {
+  subnet_id      = aws_subnet.subnet-pri-1a.id
+  route_table_id = aws_route_table.route-table-pri-1a.id
+}
+
+resource "aws_route_table_association" "route-table-ass-pri-1b" {
+  subnet_id      = aws_subnet.subnet-pri-1b.id
+  route_table_id = aws_route_table.route-table-pri-1b.id
 }
