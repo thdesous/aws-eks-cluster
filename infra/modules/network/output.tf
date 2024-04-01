c6a404861ec0fd576a0895af902113db6fe38179
Thiago de Sousa Pereira <thdesous@gmail.com>
2024-04-01 19:29:27 -0300

    feat: created output value in network module for subnet

---
+++ infra/modules/network/output.tf
@@ -0,0 +1,15 @@
+output "subnet-pub-1a" {
+  value = aws_subnet.subnet-pub-1a.id
+}
+
+output "subnet-pub-1b" {
+  value = aws_subnet.subnet-pub-1b.id
+}
+
+output "subnet-pri-1a" {
+  value = aws_subnet.subnet-pri-1a.id
+}
+
+output "subnet-pri-1b" {
+  value = aws_subnet.subnet-pri-1b.id
+}
