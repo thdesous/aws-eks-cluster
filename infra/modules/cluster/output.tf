91e960fcd96ab5215e3f6906a9fa2e742af40880
Thiago de Sousa Pereira <thdesous@gmail.com>
2024-04-02 19:57:11 -0300

    feat: added tls certificate oidc in module output

--- infra/modules/cluster/output.tf
+++ infra/modules/cluster/output.tf
@@ -5,3 +5,7 @@ output "cluster-vpc-config" {
 output "cluster-security-group-id" {
   value = aws_eks_cluster.cluster.vpc_config[0].cluster_security_group_id
 }
+
+output "oidc-issuer" {
+  value = data.tls_certificate.tls-certificate-oidc
+}
