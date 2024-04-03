2446d1cc802609fee146fc536cd4573f53dd842f
Thiago de Sousa Pereira <thdesous@gmail.com>
2024-04-03 17:33:53 -0300

    feat: management node group module added in reference list the project

--- infra/modules.tf
+++ infra/modules.tf
@@ -13,3 +13,15 @@ module "module-cluster" {
   subnet-pub-1b                         = module.module-network.subnet-pub-1b
   tags                                  = local.tags
 }
+
+module "module-mng-node-group" {
+  source                                                    = "./modules/mng-node-group"
+  project_name                                              = var.project_name
+  cluster_name                                              = module.module-cluster.cluster-name
+  managed_amazon_eks_worker_node_policy_arn                 = var.managed_amazon_eks_worker_node_policy_arn
+  managed_amazon_ec2_container_registry_readonly_policy_arn = var.managed_amazon_ec2_container_registry_readonly_policy_arn
+  managed_amazon_eks_cni_policy_arn                         = var.managed_amazon_eks_cni_policy_arn
+  subnet-pri-1a                                             = module.module-network.subnet-pri-1a
+  subnet-pri-1b                                             = module.module-network.subnet-pri-1b
+  tags                                                      = local.tags
+}
