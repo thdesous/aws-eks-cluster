07be7aaad09bc7ff7c8851719ca5a28782fa058e
Thiago de Sousa Pereira <thdesous@gmail.com>
2024-04-04 17:31:12 -0300

    feat: added kubernetes provider and exec plugin get-token

--- infra/modules/cluster/output.tf
+++ infra/modules/cluster/output.tf
@@ -17,3 +17,11 @@ output "cluster-name" {
 output "oidc-url-identity" {
   value = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
 }
+
+output "cluster-ca-cert" {
+  value = aws_eks_cluster.cluster.certificate_authority[0].data
+}
+
+output "cluster-endpoint" {
+  value = aws_eks_cluster.cluster.endpoint
+}
--- infra/provider.tf
+++ infra/provider.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.41.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.27.0"
+    }
   }
   backend "s3" {
     bucket = "s3-aws-eks-cluster"
@@ -14,4 +18,13 @@ terraform {
 provider "aws" {
   # Configuration options
   region = "us-east-1"
+}
+provider "kubernetes" {
+  host                   = module.module-cluster.cluster-endpoint
+  cluster_ca_certificate = base64decode(module.module-cluster.cluster-ca-cert)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.module-cluster.cluster-name]
+    command     = "aws"
+  }
 }
