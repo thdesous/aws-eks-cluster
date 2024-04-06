472dd0977e0861a2386b52763a93a2318b1dc951
Thiago de Sousa Pereira <thdesous@gmail.com>
2024-04-06 18:34:38 -0300

    feat: added helm provider and exec plugin get-token

--- infra/provider.tf
+++ infra/provider.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "2.17.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.8.0"
+    }
   }
   backend "s3" {
     bucket = "s3-aws-eks-cluster"
@@ -15,6 +19,7 @@ terraform {
     region = "us-east-1"
   }
 }
+
 provider "aws" {
   # Configuration options
   region = "us-east-1"
@@ -26,5 +31,16 @@ provider "kubernetes" {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", module.module-cluster.cluster-name]
     command     = "aws"
+  }
+}
+provider "helm" {
+  kubernetes {
+    host                   = module.module-cluster.cluster-endpoint
+    cluster_ca_certificate = base64decode(module.module-cluster.cluster-ca-cert)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.module-cluster.cluster-name]
+      command     = "aws"
+    }
   }
 }
